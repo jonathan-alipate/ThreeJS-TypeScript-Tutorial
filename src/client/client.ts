ab25d57a6b4b6aafcb307119aa7626e28ca08e72
Jonathan Alipate <j.alipate@gmail.com>
2020-08-25 22:36:44 +1200

    added reflection/refraction mapping

--- src/client/client.ts
+++ src/client/client.ts
@@ -23,8 +23,8 @@ const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry()
 const torusKnotGeometry: THREE.TorusKnotGeometry = new THREE.TorusKnotGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial()//{ color: 0x00ff00, wireframe: true })
 
-const texture = new THREE.TextureLoader().load("img/grid.png")
-material.map = texture
+// const texture = new THREE.TextureLoader().load("img/grid.png")
+//material.map = texture
 const envTexture = new THREE.CubeTextureLoader().load(["img/px_50.png", "img/nx_50.png", "img/py_50.png", "img/ny_50.png", "img/pz_50.png", "img/nz_50.png"])
 envTexture.mapping = THREE.CubeReflectionMapping
 envTexture.mapping = THREE.CubeRefractionMapping
