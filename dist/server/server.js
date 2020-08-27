9e59b9819286f1c1df8887dd66a9601a7e964c1f
Jonathan Alipate <j.alipate@gmail.com>
2020-08-27 18:39:09 +1200

    added character animations

--- dist/server/server.js
+++ dist/server/server.js
@@ -13,8 +13,13 @@ class App {
         const app = express_1.default();
         app.use(express_1.default.static(path_1.default.join(__dirname, '../client')));
         app.use('/build/three.module.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/build/three.module.js')));
-        app.use('/jsm/controls/PointerLockControls', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/controls/PointerLockControls.js')));
+        app.use('/jsm/controls/OrbitControls', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/controls/OrbitControls.js')));
+        app.use('/jsm/loaders/FBXLoader', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/loaders/FBXLoader.js')));
+        app.use('/jsm/libs/inflate.module.min.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/inflate.module.min.js')));
+        app.use('/jsm/curves/NURBSCurve.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/curves/NURBSCurve.js')));
+        app.use('/jsm/curves/NURBSUtils.js', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/curves/NURBSUtils.js')));
         app.use('/jsm/libs/stats.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/stats.module.js')));
+        app.use('/jsm/libs/dat.gui.module', express_1.default.static(path_1.default.join(__dirname, '../../node_modules/three/examples/jsm/libs/dat.gui.module.js')));
         this.server = new http_1.default.Server(app);
     }
     Start() {
--- src/client/client.ts
+++ src/client/client.ts
@@ -1,83 +1,108 @@
 import * as THREE from '/build/three.module.js'
-import { PointerLockControls } from '/jsm/controls/PointerLockControls'
+import { OrbitControls } from '/jsm/controls/OrbitControls'
+import { FBXLoader } from '/jsm/loaders/FBXLoader'
 import Stats from '/jsm/libs/stats.module'
+import { GUI } from '/jsm/libs/dat.gui.module'
 
 const scene: THREE.Scene = new THREE.Scene()
+const axesHelper = new THREE.AxesHelper(5)
+scene.add(axesHelper)
 
-var light = new THREE.AmbientLight();
+var light = new THREE.PointLight();
+light.position.set(2.5, 7.5, 15)
 scene.add(light);
 
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+camera.position.set(0.8, 1.4, 1.0)
 
 const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-const menuPanel = document.getElementById('menuPanel');
-const startButton = document.getElementById('startButton');
-startButton.addEventListener('click', function () {
-    controls.lock();
-}, false);
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.screenSpacePanning = true
+controls.target.set(0, 1, 0)
 
-const controls = new PointerLockControls(camera, renderer.domElement)
-//controls.addEventListener('change', () => console.log("Controls Change"))
-controls.addEventListener('lock', () => menuPanel.style.display = 'none');
-// controls.addEventListener('unlock', () => menuPanel.style.display = 'block');
+let mixer: THREE.AnimationMixer
+let modelReady = false;
+let animationActions: THREE.AnimationAction[] = []
+let activeAction: THREE.AnimationAction
+let lastAction: THREE.AnimationAction
+const fbxLoader: FBXLoader = new FBXLoader();
 
+fbxLoader.load(
+    'models/vanguard_t_choonyung.fbx',
+    (object) => {
+        object.scale.set(.01, .01, .01)
+        mixer = new THREE.AnimationMixer(object);
 
+        let animationAction = mixer.clipAction((object as any).animations[0]);
+        animationActions.push(animationAction)
+        animationsFolder.add(animations, "default")
+        activeAction = animationActions[0]
 
-const planeGeometry: THREE.PlaneGeometry = new THREE.PlaneGeometry(100, 100, 50, 50)
-const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
-const plane: THREE.Mesh = new THREE.Mesh(planeGeometry, material)
-plane.rotateX(-Math.PI / 2)
-scene.add(plane)
+        scene.add(object);
 
-let cubes: THREE.Mesh[] = new Array()
-for (let i = 0; i < 100; i++) {
-    const geo: THREE.BoxGeometry = new THREE.BoxGeometry(Math.random() * 4, Math.random() * 16, Math.random() * 4)
-    const mat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ wireframe: true })
-    switch (i % 3) {
-        case 0:
-            mat.color = new THREE.Color(0xff0000)
-            break;
-        case 1:
-            mat.color = new THREE.Color(0xffff00)
-            break;
-        case 2:
-            mat.color = new THREE.Color(0x0000ff)
-            break;
+        //add an animation from another file
+        fbxLoader.load('models/vanguard@samba.fbx',
+            (object) => {
+                console.log("loaded samba")
+
+                let animationAction = mixer.clipAction((object as any).animations[0]);
+                animationActions.push(animationAction)         
+                animationsFolder.add(animations, "samba")
+
+                //add an animation from another file
+                fbxLoader.load('models/vanguard@bellydance.fbx',
+                    (object) => {
+                        console.log("loaded bellydance")
+                        let animationAction = mixer.clipAction((object as any).animations[0]);
+                        animationActions.push(animationAction)
+                        animationsFolder.add(animations, "bellydance")
+
+                        //add an animation from another file
+                        fbxLoader.load('models/vanguard@goofyrunning.fbx',
+                            (object) => {
+                                console.log("loaded goofyrunning");
+                                (object as any).animations[0].tracks.shift() //delete the specific track that moves the object forward while running
+                                //console.dir((object as any).animations[0])
+                                let animationAction = mixer.clipAction((object as any).animations[0]);
+                                animationActions.push(animationAction)
+                                animationsFolder.add(animations, "goofyrunning")
+
+                                modelReady = true
+                            },
+                            (xhr) => {
+                                console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+                            },
+                            (error) => {
+                                console.log(error);
+                            }
+                        )
+                    },
+                    (xhr) => {
+                        console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+                    },
+                    (error) => {
+                        console.log(error);
+                    }
+                )
+            },
+            (xhr) => {
+                console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+            },
+            (error) => {
+                console.log(error);
+            }
+        )
+    },
+    (xhr) => {
+        console.log((xhr.loaded / xhr.total * 100) + '% loaded')
+    },
+    (error) => {
+        console.log(error);
     }
-    const cube = new THREE.Mesh(geo, mat)
-    cubes.push(cube)
-}
-cubes.forEach((c) => {
-    c.position.x = (Math.random() * 100) - 50
-    c.position.z = (Math.random() * 100) - 50
-    c.geometry.computeBoundingBox()
-    c.position.y = (c.geometry.boundingBox.max.y - c.geometry.boundingBox.min.y) / 2
-    scene.add(c)
-});
-
-camera.position.y = 1
-camera.position.z = 2
-
-const onKeyDown = function (event) {
-    switch (event.keyCode) {
-        case 87: // w
-            controls.moveForward(.25)
-            break;
-        case 65: // a
-            controls.moveRight(-.25)
-            break;
-        case 83: // s
-            controls.moveForward(-.25)
-            break;
-        case 68: // d
-            controls.moveRight(.25)
-            break;
-    }
-};
-document.addEventListener('keydown', onKeyDown, false);
+)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -90,10 +115,45 @@ function onWindowResize() {
 const stats = Stats()
 document.body.appendChild(stats.dom)
 
+var animations = {
+    default: function () {
+        setAction(animationActions[0])
+    },
+    samba: function () {
+        setAction(animationActions[1])
+    },
+    bellydance: function () {
+        setAction(animationActions[2])
+    },
+    goofyrunning: function () {
+        setAction(animationActions[3])
+    },
+}
+
+const setAction = (toAction: THREE.AnimationAction) => {
+    if (toAction != activeAction) {
+        lastAction = activeAction
+        activeAction = toAction
+        lastAction.stop()
+        //lastAction.fadeOut(1)
+        activeAction.reset()
+        //activeAction.fadeIn(1)
+        activeAction.play()
+    }
+}
+
+const gui = new GUI()
+const animationsFolder = gui.addFolder("Animations")
+animationsFolder.open()
+
+const clock: THREE.Clock = new THREE.Clock()
+
 var animate = function () {
     requestAnimationFrame(animate)
 
-    //controls.update()
+    controls.update()
+
+    if (modelReady) mixer.update(clock.getDelta());
 
     render()
 
